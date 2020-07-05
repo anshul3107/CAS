588d7d0dbb3c36160fa3ddba10302d901263d87a
Anshul Gupta <anshulgupta3107@gmail.com>
2020-07-06 00:01:47 +0800

    fixes

--- service-app/Controllers/email-controller.js
+++ service-app/Controllers/email-controller.js
@@ -20,65 +20,60 @@ exports.emailVerificationStatus = (req, res, next) => {
     const email = req.query && req.query.email;
     const reqKey = req.headers && req.headers.api_key;
     if (reqKey !== serverConfig.apiKey) {
-        return next(new HttpError(500, 'Unauthorised access!'));
-    } else {
-        User.findOne({email: email})
-            .then((result) => {
-                if (result) {
-                    if (result.isVerified) {
-                        return next(new HttpError(400, 'User has already verified this email id'));
-                    } else {
-                        res.json({email: result.email, isVerified: result.isVerified});
-                    }
-                } else {
-                    const newUser = new User({email: email, isVerified: false});
-                    newUser
-                        .save()
-                        .then((result) => {
-                            const token = jwt.sign({email: email}, serverConfig.jwtPrvtKey, {expiresIn: '2h'});
-                            const mailSubject = 'Email Verification via MyAPI';
-                            const htmlStr =
-                                'Please click on the ' +
-                                `<a href="http://localhost:${serverConfig.serverPort}/api/email/verify-token?token=${token}">` +
-                                'link</a> to verify your email.';
-                            const textStr =
-                                'Please click on the following link to verify your email. ' +
-                                `http://localhost:${serverConfig.serverPort}/api/email/verify-token?token=${token}">`;
-                            sendEmail({
-                                from: serverConfig.fromEmail,
-                                to: result.email,
-                                subject: mailSubject,
-                                text: textStr,
-                                html: htmlStr
-                            })
-                                .then(() => {
-                                    res.json({email: result.email, isVerified: result.isVerified});
-                                })
-                                .catch(() => {
-                                    return next(
-                                        new HttpError(
-                                            500,
-                                            "Cannot send the Email. We're looking in the issue. Please retry in sometime!"
-                                        )
-                                    );
-                                });
-                        })
-                        .catch(() => {
-                            return next(
-                                new HttpError(
-                                    500,
-                                    "Cannot update in the DB. We're looking in the issue. Please retry in sometime!"
-                                )
-                            );
-                        });
-                }
-            })
-            .catch((err) => {
-                return next(
-                    new HttpError(500, "Cannot read from the DB. We're looking in the issue. Please retry in sometime!")
-                );
-            });
+        return next(new HttpError(401, 'Unauthorised access!'));
     }
+    User.findOne({email: email})
+        .then((result) => {
+            if (result) {
+                res.json({email: result.email, isVerified: result.isVerified});
+            } else {
+                const newUser = new User({email: email, isVerified: false});
+                newUser
+                    .save()
+                    .then((result) => {
+                        const token = jwt.sign({email: email}, serverConfig.jwtPrvtKey, {expiresIn: '2h'});
+                        const mailSubject = 'Email Verification via MyAPI';
+                        const htmlStr =
+                            'Please click on the ' +
+                            `<a href="http://localhost:${serverConfig.serverPort}/api/email/verify-token?token=${token}">` +
+                            'link</a> to verify your email.';
+                        const textStr =
+                            'Please click on the following link to verify your email. ' +
+                            `http://localhost:${serverConfig.serverPort}/api/email/verify-token?token=${token}">`;
+                        sendEmail({
+                            from: serverConfig.fromEmail,
+                            to: result.email,
+                            subject: mailSubject,
+                            text: textStr,
+                            html: htmlStr
+                        })
+                            .then(() => {
+                                res.json({email: result.email, isVerified: result.isVerified});
+                            })
+                            .catch(() => {
+                                return next(
+                                    new HttpError(
+                                        500,
+                                        "Cannot send the Email. We're looking in the issue. Please retry in sometime!"
+                                    )
+                                );
+                            });
+                    })
+                    .catch(() => {
+                        return next(
+                            new HttpError(
+                                500,
+                                "Cannot update in the DB. We're looking in the issue. Please retry in sometime!"
+                            )
+                        );
+                    });
+            }
+        })
+        .catch((err) => {
+            return next(
+                new HttpError(500, "Cannot read from the DB. We're looking in the issue. Please retry in sometime!")
+            );
+        });
 };
 
 exports.emailVerificationUpdate = (req, res, next) => {
@@ -88,7 +83,7 @@ exports.emailVerificationUpdate = (req, res, next) => {
         User.findOne({email: decoded.email}).then((result) => {
             if (result) {
                 if (result.isVerified) {
-                    return next(new HttpError(400, 'User has already verified this email id'));
+                    res.json({email: result.email, isVerified: result.isVerified});
                 } else {
                     User.findOneAndUpdate({email: decoded.email}, {$set: {isVerified: true}})
                         .then((result) => {
@@ -99,7 +94,7 @@ exports.emailVerificationUpdate = (req, res, next) => {
                         });
                 }
             } else {
-                return next(new HttpError(404, "Specified user doesn't exists!"));
+                return next(new HttpError(404, "Specified user doesn't exist!"));
             }
         });
     } catch (err) {
