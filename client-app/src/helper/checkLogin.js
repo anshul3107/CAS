c75165feeabfcb5f1d2013d5ff00ed8b59199f54
anshul3107 <anshulgupta3107@gmail.com>
2020-08-10 16:25:59 +0100

    Bug fixes

--- client-app/src/helper/checkLogin.js
+++ client-app/src/helper/checkLogin.js
@@ -1,9 +1,10 @@
 export const checkLogin = () => {
-  if (
-    !window.localStorage.getItem('authData') &&
-    window.location.pathname !== '/user/login' &&
-    window.location.pathname !== '/user/register'
-  ) {
-    window.location = '/user/login';
-  }
+    if (
+        !window.localStorage.getItem('authData') &&
+        window.location.pathname !== '/user/login' &&
+        window.location.pathname !== '/user/register' &&
+        window.location.pathname !== '/verify/token'
+    ) {
+        window.location = '/user/login';
+    }
 };
