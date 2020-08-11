e8271e1aef482897eb02bf013872c01f859de62a
anshul3107 <anshulgupta3107@gmail.com>
2020-08-11 15:27:02 +0100

    Page Redirection fix

--- client-app/src/helper/checkLogin.js
+++ client-app/src/helper/checkLogin.js
@@ -3,7 +3,9 @@ export const checkLogin = () => {
         !window.localStorage.getItem('authData') &&
         window.location.pathname !== '/user/login' &&
         window.location.pathname !== '/user/register' &&
-        window.location.pathname !== '/verify/token'
+        window.location.pathname !== '/verify/token' &&
+        window.location.pathname !== '/forgot/password' &&
+        window.location.pathname !== '/account/password'
     ) {
         window.location = '/user/login';
     }
