c549a5d63830268218ec24907c936261a941926c
Anshul Gupta <anshulgupta3107@gmail.com>
2020-08-12 22:00:25 +0800

    fB login integration

--- client-app/src/helper/checkLogin.js
+++ client-app/src/helper/checkLogin.js
@@ -1,12 +1,13 @@
 export const checkLogin = () => {
-    if (
-        !window.localStorage.getItem('authData') &&
-        window.location.pathname !== '/user/login' &&
-        window.location.pathname !== '/user/register' &&
-        window.location.pathname !== '/verify/token' &&
-        window.location.pathname !== '/forgot/password' &&
-        window.location.pathname !== '/account/password'
-    ) {
-        window.location = '/user/login';
-    }
+  if (
+    !window.localStorage.getItem('authData') &&
+    window.location.pathname !== '/user/login' &&
+    window.location.pathname !== '/user/register' &&
+    window.location.pathname !== '/verify/token' &&
+    window.location.pathname !== '/forgot/password' &&
+    window.location.pathname !== '/account/password' &&
+    window.location.pathname !== '/user/socialprofile'
+  ) {
+    window.location = '/user/login';
+  }
 };
